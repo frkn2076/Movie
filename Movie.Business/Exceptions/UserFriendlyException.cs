bf7b2ec1a32535b637c566ed45953c49b5c406f7
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-11-18 02:18:39 +0300

    Added user-friendly eceptions

---
+++ Movie.Business/Exceptions/UserFriendlyException.cs
@@ -0,0 +1,27 @@
+﻿namespace Movie.Business.Exceptions
+{
+    public class NoMovieFound : Exception
+    {
+        public NoMovieFound() : base("No movie found") { }
+    }
+
+    public class MovieInfoNotFound : Exception
+    {
+        public MovieInfoNotFound() : base("Movie info not found") { }
+    }
+
+    public class NoMoviePosterFound : Exception
+    {
+        public NoMoviePosterFound() : base("No movie poster found") { }
+    }
+
+    public class NoMovieDescriptionFound : Exception
+    {
+        public NoMovieDescriptionFound() : base("No movie description found") { }
+    }
+
+    public class NoWatchListFoundForUser : Exception
+    {
+        public NoWatchListFoundForUser() : base("No watchlist found for user") { }
+    }
+}
