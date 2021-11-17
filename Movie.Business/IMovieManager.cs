91a1e7d22989b74106d0f50366798ba1737c2bba
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-11-18 02:19:11 +0300

    Added business manager

--- Movie.Business/IMovieManager.cs
+++ Movie.Business/IMovieManager.cs
@@ -1,10 +1,14 @@
-﻿using Movie.DataAccess.Entity;
+﻿using Movie.Business.DTO;
 
 namespace Movie.Business
 {
     public interface IMovieManager
     {
-        public Task AddMovie(WatchList watchList);
-        public List<WatchList> GetMovies();
+        public Task<List<MovieDTO>> SearcyMovieByName(string name);
+        public Task<MovieInfoDTO> GetInfoByMovieId(string id);
+        public Task<List<MoviePosterDTO>> GetMoviePostersById(string id);
+        public Task<DescriptionDTO> GetMovieDescriptionById(string id);
+        public Task AddMovieToWatchListOfUser(MovieDetailDTO movie);
+        public Task<List<WatchListDTO>> GetWatchListOfUser(int userId);
     }
 }
---
+++ Movie.Business/MovieDetailDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Movie.Business
+{
+    public class MovieDetailDTO
+    {
+        public int UserId { get; set; }
+        public string MovieId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
