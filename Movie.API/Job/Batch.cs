c3f88424d5b545a76304f55b497af3910555826e
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-11-19 01:29:20 +0300

    added scheduled job

---
+++ Movie.API/Job/Batch.cs
@@ -0,0 +1,35 @@
+﻿using Movie.Business;
+using Movie.Business.Notifier;
+
+namespace Movie.API.Job
+{
+    public static class Batch
+    {
+        private static IMovieManager _movieManager;
+        public static void Configure(IMovieManager movieManager) => _movieManager = movieManager;
+
+        public async static Task SendMostRatedMovie(string to)
+        {
+            // userId set as 1 since there is no saved user
+            var movieIds = await _movieManager.GetMostRatedMovieIds(1);
+            if (movieIds == null || !movieIds.Any())
+                return;
+
+            var tasks = movieIds.Select(x => _movieManager.GetInfoByMovieId(x)).ToList();
+
+            var responses = await Task.WhenAll(tasks);
+
+            var mostRatedMovie = responses.Where(x => x != null).MaxBy(x => Convert.ToDouble(x.Rating));
+
+            if (mostRatedMovie == null)
+                return;
+
+            var posters = await _movieManager.GetMoviePostersById(mostRatedMovie.Id);
+            var poster = posters.FirstOrDefault(x => !string.IsNullOrWhiteSpace(x.Link));
+
+            var movieDescription = await _movieManager.GetMovieDescriptionById(mostRatedMovie.Id);
+
+            MailSender.SendMail(to, mostRatedMovie.Title, mostRatedMovie.Rating, poster?.Link, movieDescription.Html);
+        }
+    }
+}
---
+++ Movie.API/Job/HangfireJobs.cs
@@ -0,0 +1,10 @@
+﻿using Hangfire;
+
+namespace Movie.API.Job;
+public static class HangfireJobs
+{
+    public static void Init()
+    {
+        RecurringJob.AddOrUpdate("myrecurringjob", () => Batch.SendMostRatedMovie("ozturkfurkan1994@hotmail.com"), "0 30 19 ? * SUN");
+    }
+}
