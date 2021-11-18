3c6639472b99e33bb22a08106a10a247f7f1248f
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-11-19 01:35:18 +0300

    reformatted

--- Movie.API/Job/Batch.cs
+++ Movie.API/Job/Batch.cs
@@ -1,35 +1,33 @@
 ﻿using Movie.Business;
 using Movie.Business.Notifier;
 
-namespace Movie.API.Job
+namespace Movie.API.Job;
+public static class Batch
 {
-    public static class Batch
+    private static IMovieManager _movieManager;
+    public static void Configure(IMovieManager movieManager) => _movieManager = movieManager;
+
+    public async static Task SendMostRatedMovie(string to)
     {
-        private static IMovieManager _movieManager;
-        public static void Configure(IMovieManager movieManager) => _movieManager = movieManager;
+        // userId set as 1 since there is no saved user
+        var movieIds = await _movieManager.GetMostRatedMovieIds(1);
+        if (movieIds == null || !movieIds.Any())
+            return;
 
-        public async static Task SendMostRatedMovie(string to)
-        {
-            // userId set as 1 since there is no saved user
-            var movieIds = await _movieManager.GetMostRatedMovieIds(1);
-            if (movieIds == null || !movieIds.Any())
-                return;
+        var tasks = movieIds.Select(x => _movieManager.GetInfoByMovieId(x)).ToList();
 
-            var tasks = movieIds.Select(x => _movieManager.GetInfoByMovieId(x)).ToList();
+        var responses = await Task.WhenAll(tasks);
 
-            var responses = await Task.WhenAll(tasks);
+        var mostRatedMovie = responses.Where(x => x != null).MaxBy(x => Convert.ToDouble(x.Rating));
 
-            var mostRatedMovie = responses.Where(x => x != null).MaxBy(x => Convert.ToDouble(x.Rating));
+        if (mostRatedMovie == null)
+            return;
 
-            if (mostRatedMovie == null)
-                return;
+        var posters = await _movieManager.GetMoviePostersById(mostRatedMovie.Id);
+        var poster = posters.FirstOrDefault(x => !string.IsNullOrWhiteSpace(x.Link));
 
-            var posters = await _movieManager.GetMoviePostersById(mostRatedMovie.Id);
-            var poster = posters.FirstOrDefault(x => !string.IsNullOrWhiteSpace(x.Link));
+        var movieDescription = await _movieManager.GetMovieDescriptionById(mostRatedMovie.Id);
 
-            var movieDescription = await _movieManager.GetMovieDescriptionById(mostRatedMovie.Id);
-
-            MailSender.SendMail(to, mostRatedMovie.Title, mostRatedMovie.Rating, poster?.Link, movieDescription.Html);
-        }
+        MailSender.SendMail(to, mostRatedMovie.Title, mostRatedMovie.Rating, poster?.Link, movieDescription.Html);
     }
 }
