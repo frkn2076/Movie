ed419329dfd6364c15677c8a13e1b7c4f38019f8
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-11-19 01:30:50 +0300

    added fluent validator

---
+++ Movie.API/Validators/MovieCollectionRequestValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+using Movie.API.Requests;
+
+namespace Movie.API.Validators;
+public class MovieCollectionRequestValidator : AbstractValidator<MovieCollectionRequest>
+{
+    public MovieCollectionRequestValidator()
+    {
+        RuleFor(p => p.MovieNames).NotEmpty();
+    }
+}
